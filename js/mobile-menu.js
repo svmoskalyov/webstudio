34f8ec2ebaa6c9cc1a7e4178a9cda74eb4e2b6ca
Serhii Moskalov <sv.moskalyov@gmail.com>
2023-03-29 23:00:39 +0300

    Create mobile-menu.js

---
+++ js/mobile-menu.js
@@ -0,0 +1,7 @@
+document.querySelector('.menu-btn').addEventListener('click',()=> {
+document.querySelector('.mobile-menu').classList.toggle('active');
+document.querySelector('.menu-btn').classList.toggle('active');
+document.body.classList.toggle('overflow');
+});
+
+
